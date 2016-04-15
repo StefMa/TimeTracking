2ef4269b7c71a04b360c82f50fd7563e6793adef
StefMa <StefMaDev@outlook.com>
2016-04-15 08:05:01 +0200

    Remove Time Track on remove view click

--- app/src/main/java/guru/stefma/timetracking/AddTimeTrackActivity.java
+++ app/src/main/java/guru/stefma/timetracking/AddTimeTrackActivity.java
@@ -4,6 +4,7 @@ import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -13,12 +14,16 @@ import android.widget.TimePicker;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 
+import java.util.ArrayList;
+
 public class AddTimeTrackActivity extends AppCompatActivity
         implements TimePickerDialog.OnTimeSetListener {
 
     private static final String KEY_DAY = "KEY_DAY";
 
     private LinearLayout mTimeTrackContainer;
+
+    private ArrayList<TimeTrackView> mTimTracks = new ArrayList<>();
 
     public static Intent newInstance(Context context, CalendarDay date) {
         Intent intent = new Intent();
@@ -38,6 +43,8 @@ public class AddTimeTrackActivity extends AppCompatActivity
         mTimeTrackContainer = (LinearLayout) findViewById(R.id.addtimetrack_time_track_container);
         setupFab();
 
+        addNewTimeTrack();
+
         showTimePickerDialog();
     }
 
@@ -53,14 +60,31 @@ public class AddTimeTrackActivity extends AppCompatActivity
     }
 
     private void addNewTimeTrack() {
-        TimeTrackView trackView = new TimeTrackView(this);
+        final TimeTrackView trackView = new TimeTrackView(this);
         ViewGroup.MarginLayoutParams layoutParams =
                 new LinearLayout.LayoutParams(
                         ViewGroup.LayoutParams.MATCH_PARENT,
                         ViewGroup.LayoutParams.WRAP_CONTENT);
         layoutParams.setMargins(0, (int) getResources().getDimension(R.dimen.small), 0, 0);
         trackView.setLayoutParams(layoutParams);
+        trackView.setOnRemoveClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                removeTimeTrack(view, trackView);
+            }
+        });
         mTimeTrackContainer.addView(trackView);
+        mTimTracks.add(trackView);
+    }
+
+    private void removeTimeTrack(View view, TimeTrackView trackView) {
+        if (mTimeTrackContainer.getChildCount() > 1) {
+            mTimeTrackContainer.removeView(trackView);
+            mTimTracks.remove(trackView);
+        } else {
+            Snackbar.make(view, getString(R.string.error_remove_track_view),
+                    Snackbar.LENGTH_SHORT).show();
+        }
     }
 
     private void showTimePickerDialog() {
--- app/src/main/java/guru/stefma/timetracking/TimeTrackView.java
+++ app/src/main/java/guru/stefma/timetracking/TimeTrackView.java
@@ -3,8 +3,11 @@ package guru.stefma.timetracking;
 import android.content.Context;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
+import android.view.View;
 
 public class TimeTrackView extends CardView {
+
+    private View mTimeTrackRemove;
 
     public TimeTrackView(Context context) {
         this(context, null);
@@ -17,6 +20,12 @@ public class TimeTrackView extends CardView {
         setRadius(getResources().getDimension(R.dimen.cardview_default_radius));
 
         inflate(context, R.layout.view_time_track, this);
+
+        mTimeTrackRemove = findViewById(R.id.time_track_remove);
+    }
+
+    public void setOnRemoveClickListener(View.OnClickListener clickListener) {
+        mTimeTrackRemove.setOnClickListener(clickListener);
     }
 
 }
